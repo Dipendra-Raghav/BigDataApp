e1a5f3dab6e9260bda89da5ee2d272ef343d8dc6
Dipendra-Raghav <raghavdip19@gmail.com>
2021-11-03 09:24:26 +0530

    phone no page designed

--- app/src/main/java/com/example/bigdata/DashBoard.java
+++ app/src/main/java/com/example/bigdata/DashBoard.java
@@ -5,13 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 public class DashBoard extends AppCompatActivity {
+    RelativeLayout chat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+        chat =findViewById(R.id.chat);
+
+        chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),PhoneNumberActivity.class));
+                finish();
+
+            }
+        });
     }
 
     public void profile(View view) {
